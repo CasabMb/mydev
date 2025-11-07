074fdd8bf05c02761bd8b7b33ab9fa7d2210beec
minma <casabianca.marie.b@gmail.com>
2025-11-07 19:40:00 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: true, // ✅ active la minification HTML + JS inline
+});
