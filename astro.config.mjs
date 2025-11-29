c95f300d7e3d5deca98d314cbe3de0fde2aeb290
Casabianca Marie <casab@MacBook-Pro-de-Casabianca.local>
2025-11-29 13:56:06 +0100

    deploy test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true, // ✅ active la minification HTML + JS inline
+  site: 'https://casabmb.github.io',
+  base: '/mydev/'
 });
